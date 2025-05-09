0c788db9c82d7b31a1f71ebb6014338f3e3cc5fd
Ahmed-Raza001 <ahmadrazaharsoliya67@gmail.com>
2025-05-09 05:42:56 +0000

    js code

---
+++ script.js
@@ -0,0 +1,102 @@
+/*let a = 10;
+a = 5;
+
+console.log(a);
+
+let b = 7 * a;
+console.log(b);*/
+
+/*const max = 57;
+const actual = max - 13;
+const percentage = actual / max;*/
+
+/*console.log(percentage);
+
+let x = 5;
+x += 10;
+
+console.log (x);*/
+
+/*function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!"
+}
+
+console.log(favoriteAnimal('Cat'))*/
+
+/*const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);*/
+
+/*function myFunction() {
+    alert("hello");
+  }
+  
+  myFunction();*/
+  // calls the function once
+  
+/*const myArray = ["I", "love", "chocolate", "frogs"];
+const madeAString = myArray.join(" ");
+console.log(madeAString);
+// returns 'I love chocolate frogs'
+
+const madeAnotherString = myArray.join();
+console.log(madeAnotherString);
+// returns 'I,love,chocolate,frogs'*/
+
+
+/*let userName = 'John';
+
+function showMessage() {
+  let message = 'Hello, ' + userName;
+  alert(message);
+}
+
+showMessage();*/
+
+ // Function 1: Simple greeting
+ /*function sayHello() {
+    console.log("Hello there!");
+  }
+
+  // Function 2: Favorite animal
+  function favoriteAnimal(animal) {
+    return animal + " is my favorite animal!";
+  }
+
+  // Function 3: Add two numbers
+  function addNumbers(a, b) {
+    return a + b;
+  }
+
+  // Call the functions
+  sayHello();
+  console.log(favoriteAnimal("Cat"));
+  console.log(addNumbers(5, 5));*/
+
+  // Function 1: add7 - returns the number + 7
+/*function add7(num) {
+    return num + 7;
+  }
+  console.log(add7(5)); // Output: 12
+  
+  // Function 2: multiply - returns the product of two numbers
+  function multiply(a, b) {
+    return a * b;
+  }
+  console.log(multiply(3, 4)); // Output: 12
+  
+  // Function 3: capitalize - capitalizes the first letter of the string
+  function capitalize(str) {
+    if (str.length === 0) return "";
+    return str[0].toUpperCase() + str.slice(1).toLowerCase();
+  }
+  console.log(capitalize("hello"));     // Output: "Hello"
+  console.log(capitalize("HELLO"));     // Output: "Hello"
+  console.log(capitalize("hELlo"));     // Output: "Hello"
+  
+  // Function 4: lastLetter - returns the last letter of the string
+  function lastLetter(str) {
+    return str[str.length - 1];
+  }
+  console.log(lastLetter("abcd")); // Output: "d"*/
+  
